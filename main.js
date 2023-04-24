fbacb356d43a25c1f5378713eccd8a9243048880
MercyEarls <114352112+MercyEarls@users.noreply.github.com>
2023-04-24 23:27:38 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+// alert('working');
+const rating1 = document.getElementById('rating1')
+
+rating1.addEventListener('mouseover', function () {
+    rating1.style.backgroundColor = 'hsl(25, 97%, 53%)'
+    rating1.style.color = 'hsl(0, 0%, 100%)'
+})
+
+const rating2 = document.getElementById('rating2')
+
+rating2.addEventListener('mouseover', function () {
+    rating2.style.backgroundColor = 'hsl(25, 97%, 53%)'
+    rating2.style.color = 'hsl(0, 0%, 100%)'
+})
+
+const rating3 = document.getElementById('rating3')
+
+rating3.addEventListener('mouseover', function () {
+    rating3.style.backgroundColor = 'hsl(25, 97%, 53%)'
+    rating3.style.color = 'hsl(0, 0%, 100%)'
+})
+
+const rating4 = document.getElementById('rating4')
+
+rating4.addEventListener('mouseover', function () {
+    rating4.style.backgroundColor = 'hsl(25, 97%, 53%)'
+    rating4.style.color = 'hsl(0, 0%, 100%)'
+})
+
+const rating5 = document.getElementById('rating5')
+
+rating5.addEventListener('mouseover', function () {
+    rating5.style.backgroundColor = 'hsl(25, 97%, 53%)'
+    rating5.style.color = 'hsl(0, 0%, 100%)'
+})
+
+console.log('I am getting it right')
+
+
+function redirect(index){
+    window.location.href = index
+}
+
+console.log('I am getting it right')
+
+
+function redirect(thanks){
+    window.location.href = thanks
+}
+
+
+const butt = document.getElementById('sub')
+
+butt.addEventListener('mouseover', function (){
+butt.style.backgroundColor = 'hsl(0, 0%, 100%)'
+butt.style.color = 'hsl(25, 97%, 53%)'
+
+})
